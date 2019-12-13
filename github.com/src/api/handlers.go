d8bf71e5bd5321e543ebf6a60e582d5d039f4731
fuqiang <18251359382@163.com>
2019-12-13 23:17:16 +0800

    api test

--- github.com/src/api/handlers.go
+++ github.com/src/api/handlers.go
@@ -9,3 +9,8 @@ import (
 func CreatUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	io.WriteString(w, "Create user handler")
 }
+
+func Login(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	uname := p.ByName("user_name")
+	io.WriteString(w, uname)
+}
--- github.com/src/api/main.go
+++ github.com/src/api/main.go
@@ -7,7 +7,10 @@ import (
 
 func RegisterHandles() *httprouter.Router {
 	router := httprouter.New()
+	//创建用户
 	router.POST("/user", CreatUser)
+	//用户登录
+	router.POST("/user/:user_name", Login)
 	return router
 }
 
