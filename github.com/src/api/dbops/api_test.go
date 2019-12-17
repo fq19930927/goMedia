e0b7123b8de1d055e7309c8ebcaab18db2d48269
fuqiang <18251359382@163.com>
2019-12-17 16:27:48 +0800

    add test case

--- github.com/src/api/dbops/api_test.go
+++ github.com/src/api/dbops/api_test.go
@@ -1,6 +1,11 @@
 package dbops
 
-import "testing"
+import (
+	"fmt"
+	"strconv"
+	"testing"
+	"time"
+)
 
 func clearTables() {
 	dbConn.Exec("truncate users")
@@ -41,5 +46,62 @@ func testDeleteUser(t *testing.T) {
 	err := DeleteUser("fuqiang", "123")
 	if err != nil {
 		t.Logf("delete user error is %v", err)
+	}
+}
+
+func TestVideoWorkFlow(t *testing.T) {
+
+}
+
+var tempvid string
+
+func testAddVideInfo(t *testing.T) {
+	vi, err := AddNewVideo(1, "my-video")
+	if err != nil {
+		t.Errorf("Eroor or AddVideoInfo: %v", err)
+	}
+	tempvid = vi.Id
+}
+
+func testGetVideoInfo(t *testing.T) {
+	_, err := GetVideoInfo(tempvid)
+	if err != nil {
+		t.Errorf("Eroor or GetVideoInfo: %v", err)
+	}
+}
+
+func TestDeleteVideoInfo(t *testing.T) {
+	err := DeleteVideoInfo(tempvid)
+	if err != nil {
+		t.Errorf("Eroor or DeleteVideoInfo: %v", err)
+	}
+}
+
+func TestCommentsFlow(t *testing.T) {
+
+}
+
+func testAddComments(t *testing.T) {
+	vid := "12345"
+	aid := 1
+	content := "I like this video"
+
+	err := AddNewComments(vid, aid, content)
+
+	if err != nil {
+		t.Errorf("Error of AddComments: %v", err)
+	}
+}
+
+func testListComments(t *testing.T) {
+	vid := "12345"
+	from := 1514764800
+	to, _ := strconv.Atoi(strconv.FormatInt(time.Now().UnixNano()/1000000000, 10))
+	res, err := ListComments(vid, from, to)
+	if err != nil {
+		t.Errorf("Error of ListComments: %v", err)
+	}
+	for i, ele := range res {
+		fmt.Printf("comment: %d, %v \n", i, ele)
 	}
 }
