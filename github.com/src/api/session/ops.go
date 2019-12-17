5d402098abe327218ec87f46a2b957b5d76b9b01
fuqiang <18251359382@163.com>
2019-12-17 19:40:17 +0800

    完善session api

--- github.com/src/api/session/ops.go
+++ github.com/src/api/session/ops.go
@@ -1,6 +1,12 @@
 package session
 
-import "sync"
+import (
+	"../dbops"
+	"../defs"
+	"../util"
+	"sync"
+	"time"
+)
 
 var sessionMap *sync.Map
 
@@ -8,14 +14,58 @@ func init() {
 	sessionMap = &sync.Map{}
 }
 
-func LoadSessionsFromDB() {
+func nowInMilli() int64 {
+	return time.Now().UnixNano() / 1000000
+}
 
+func LoadSessionsFromDB() {
+	result, err := dbops.RetrieveAllSessions()
+	if err != nil {
+		return
+	}
+	result.Range(func(k, v interface{}) bool {
+		ss := v.(*defs.SimpleSession)
+		sessionMap.Store(k, ss)
+		return true
+	})
 }
 
 func GenerateNewSessionId(un string) string {
+	id, _ := util.NewUUID()
+	ct := nowInMilli()
+	//定义session过期时间30分钟
+	ttl := ct + 30*60*1000
+	ss := &defs.SimpleSession{Username: un, TTL: ttl}
+	sessionMap.Store(id, ss)
+	dbops.InsertSession(id, ttl, un)
+	return id
 
 }
 
-func IsSessionExpired(sid string) (string, bool) {
+func deleteExpiredSession(sid string) {
+	sessionMap.Delete(sid)
+	dbops.DeleteSession(sid)
+}
 
+func IsSessionExpired(sid string) (string, bool) {
+	ss, ok := sessionMap.Load(sid)
+	ct := nowInMilli()
+	if ok {
+		if ss.(defs.SimpleSession).TTL < ct {
+			deleteExpiredSession(sid)
+			return "", true
+		}
+		return ss.(*defs.SimpleSession).Username, false
+	} else {
+		ss, err := dbops.RetrieveSession(sid)
+		if err != nil || ss == nil {
+			return "", true
+		}
+		if ss.TTL < ct {
+			deleteExpiredSession(sid)
+			return "", true
+		}
+		sessionMap.Store(sid, ss)
+		return ss.Username, false
+	}
 }
